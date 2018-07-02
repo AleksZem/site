42417b191386b44c903c9f3bb5c178e46b13236f
aleks <alekszemlyanskiy@gmail.com>
2018-07-02 14:56:43 -0700

    Early stage remake

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 $(document).ready(function(){
-	$(".hidden").fadeIn(1500);
+	$(".hidden").fadeIn(750);
 });
 
-$("img").on("click",
-	function(){
-		console.log("Would you mind not doing that?");
-	}
-);
+jQuery(function ($, undefined) {
+	$('#footer').terminal(function (command) {
+		if (command !== '') {
+			if(command.toLowerCase().indexOf("sudo")>=0){
+				this.echo("Come on you goofball, you know this isn't a unix shell");
+				return;
+			}
+			if(command === 'help'){
+				this.echo("Okay I lied, I haven't built in any functionality at all at this point");
+				return;
+			}
+			try {
+				var result = window.eval(command);
+				if (result !== undefined) {
+					this.echo(new String(result));
+				}
+			} catch (e) {
+				this.error(new String(e) + ', if you need help just try yelling for a while');
+			}
+		} else {
+			this.echo('');
+		}
+	}, {
+			greetings: 'Type \'help\' for more instructions',
+			name: 'JS_Trash',
+			height: 200,
+			prompt: 'PoorOpSec@root> '
+		});
+});
